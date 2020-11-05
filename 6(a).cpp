c7cc9e127d44fd0c6ba064ff4ebaed5cad46e2de
KamilKowalczyk8 <61974258+KamilKowalczyk8@users.noreply.github.com>
2020-11-05 14:36:38 +0100

    Update 6(a).cpp

--- 6(a).cpp
+++ 6(a).cpp
@@ -10,24 +10,28 @@ int main(int argc, char** argv)
 {
  ifstream file; //plik wejsciowy
  ofstream fileOut; //plik wynikowy do zapisu
- //char* endptr; // to jest wskaünik na miejsce, gdzie przy konwersji z systemu czworkowego wystπpi b≥πd - nieuøywana zmienna
- //long liczba;
-int wynik=0; //liczba dziesietna, minimum z pliku
- //string liczba1;
- string s; //dwie liczby w systemie czworkowym wczytujemy jako "string" bo mogπ byÊ d≥uøsze od long long
+ //char* endptr; // to jest wska≈∫nik na miejsce, gdzie przy konwersji z systemu czworkowego wystƒÖpi b≈ÇƒÖd - nieu≈ºywana zmienna
+ long liczba;
+ long wynik=0; //liczba dziesietna, minimum z pliku
+ string liczba1;
+ string s; //dwie liczby w systemie czworkowym wczytujemy jako "string" bo mogƒÖ byƒá d≈Çu≈ºsze od long long
  file.open("dane123.txt"); //otworz plik liczb
- fileOut.open("wyjscie_1234.txt"); //otworz plik do wynikow  //do algorytmu MINIMUM - zak≥adamy, øe pierwsza liczba jest najmniejsza z dotychczas przeczytanych
+ fileOut.open("wyjscie_123.txt"); //otworz plik do wynikow  //do algorytmu MINIMUM - zak≈Çadamy, ≈ºe pierwsza liczba jest najmniejsza z dotychczas przeczytanych
+ 
 
      while(!file.eof())        //petla wykonuje sie az program dojedzie do konca pliku
      {
-         if(s[0]==s[s.length()-1])wynik++;
+              file>>liczba1;
+              liczba =  strtol(liczba1.c_str(), NULL, 8);
+               //^konwersja z systemu czworkowego do dziesietnego, string (C++) trzeba zamieniƒá na char * (C) funkcja .c_str()
+               //zmienna endptr wskazuje na znak w stringu, kt√≥ry nie pasuje do systemu czw√≥rkowego - tu niesprawdzone, bo zak≈Çadamy, ≈ºe dane sƒÖ poprawne!
+               if (s[0] == s[s.length()-1]) //je≈õli to pierwsza liczba z pliku - bƒôdzie nowym MINIMUM
+               {
+                            wynik++;
+               }
+
      }
-     string str(s);
-    for(unsigned i=0; i<str.length(); i++)
-    {
-        if(str[0]==str[str.length()-1])wynik++;
-    }
-    cout<<wynik<<endl;
+      cout<< wynik;
  fileOut<< "Wynik" <<" -- "<< wynik <<endl;
 
  file.close();
